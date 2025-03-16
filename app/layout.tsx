a776c0d4886ee8da1d0cdbe9837d9dc9c4e13775
Nathan Rose <contacttstyle@gmail.com>
2025-03-16 02:12:25 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-// app/layout.tsx
 import './css/style.css'
 import { Inter } from 'next/font/google'
-import { AuthProvider } from './context/auth-context'
+import ClientLayout from './client-layout'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -22,11 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.variable} font-inter antialiased bg-slate-900 text-slate-100 tracking-tight`}>
-        <AuthProvider>
-          <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
-            {children}
-          </div>
-        </AuthProvider>
+        <ClientLayout>
+          {children}
+        </ClientLayout>
       </body>
     </html>
   )
