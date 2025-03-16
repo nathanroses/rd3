e347d6ee99a2474828aa69473efb2e29032eeeb0
Nathan Rose <contacttstyle@gmail.com>
2025-03-16 14:58:00 -0500

    Update pricing-section.tsx

--- app/(default)/pricing-section.tsx
+++ app/(default)/pricing-section.tsx
@@ -19,7 +19,7 @@ export default function PricingSection() {
               <div className="inline-flex font-medium bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-200 pb-3">Pricing</div>
             </div>
             <h2 className="h2 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">Solutions that scale with your needs</h2>
-            <p className="text-lg text-slate-400">Browse options below or contact us for a custom package tailored to your specific requirements.</p>
+            <p className="text-lg text-slate-400">Choose your plan below, or talk to us about custom enterprise solutions.</p>
           </div>
           <Pricing />
         </div>
