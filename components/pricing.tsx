b5f7fc32c315c711057fcf5f1c4b4c52ac26a639
Nathan Rose <contacttstyle@gmail.com>
2025-03-19 15:31:16 -0500

    Update pricing.tsx

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -101,14 +101,14 @@ export default function Pricing() {
         </div>
         {/* Customers */}
         <div className="px-6 flex flex-col justify-end">
-          <div className="py-2 text-slate-400 border-b border-slate-800">Data</div>
+          <div className="py-2 text-slate-400 border-b border-slate-800">Data Processing</div>
         </div>
         <div className="px-6 flex flex-col justify-end">
           <div className="flex items-center h-full border-b border-slate-800 py-2 text-slate-400">
             <svg className="shrink-0 fill-purple-500 mr-3" xmlns="http://www.w3.org/2000/svg" width="12" height="9">
               <path d="M10.28.28 3.989 6.575 1.695 4.28A1 1 0 0 0 .28 5.695l3 3a1 1 0 0 0 1.414 0l7-7A1 1 0 0 0 10.28.28Z" />
             </svg>
-            <span>50 GB<span className="md:hidden">Data</span></span>
+            <span>50 GB<span className="md:hidden">Data Processing</span></span>
           </div>
         </div>
         <div className="px-6 flex flex-col justify-end">
@@ -116,7 +116,7 @@ export default function Pricing() {
             <svg className="shrink-0 fill-purple-500 mr-3" xmlns="http://www.w3.org/2000/svg" width="12" height="9">
               <path d="M10.28.28 3.989 6.575 1.695 4.28A1 1 0 0 0 .28 5.695l3 3a1 1 0 0 0 1.414 0l7-7A1 1 0 0 0 10.28.28Z" />
             </svg>
-            <span>250 GB<span className="md:hidden">Data</span></span>
+            <span>250 GB<span className="md:hidden">Data Processing</span></span>
           </div>
         </div>
         <div className="px-6 flex flex-col justify-end">
@@ -124,12 +124,12 @@ export default function Pricing() {
             <svg className="shrink-0 fill-purple-500 mr-3" xmlns="http://www.w3.org/2000/svg" width="12" height="9">
               <path d="M10.28.28 3.989 6.575 1.695 4.28A1 1 0 0 0 .28 5.695l3 3a1 1 0 0 0 1.414 0l7-7A1 1 0 0 0 10.28.28Z" />
             </svg>
-            <span>1 TB<span className="md:hidden">Data</span></span>
+            <span>1 TB<span className="md:hidden">Data Processing</span></span>
           </div>
         </div>
         {/* Storage Slots */}
         <div className="px-6 flex flex-col justify-end">
-          <div className="py-2 text-slate-400 border-b border-slate-800">Data Storage</div>
+          <div className="py-2 text-slate-400 border-b border-slate-800">Storage</div>
         </div>
         <div className="px-6 flex flex-col justify-end">
           <div className="flex items-center h-full border-b border-slate-800 py-2 text-slate-400">
@@ -192,7 +192,7 @@ export default function Pricing() {
             <svg className="shrink-0 fill-purple-500 mr-3" xmlns="http://www.w3.org/2000/svg" width="12" height="9">
               <path d="M10.28.28 3.989 6.575 1.695 4.28A1 1 0 0 0 .28 5.695l3 3a1 1 0 0 0 1.414 0l7-7A1 1 0 0 0 10.28.28Z" />
             </svg>
-            <span>Basic anomaly detection<span className="md:hidden">AI Integrations</span></span>
+            <span>Basic Anomaly Detection<span className="md:hidden">AI Integrations</span></span>
           </div>
         </div>
         <div className="px-6 flex flex-col justify-end">
@@ -200,7 +200,7 @@ export default function Pricing() {
             <svg className="shrink-0 fill-purple-500 mr-3" xmlns="http://www.w3.org/2000/svg" width="12" height="9">
               <path d="M10.28.28 3.989 6.575 1.695 4.28A1 1 0 0 0 .28 5.695l3 3a1 1 0 0 0 1.414 0l7-7A1 1 0 0 0 10.28.28Z" />
             </svg>
-            <span>Trend forecasting<span className="md:hidden">AI Integrations</span></span>
+            <span>Trend Forecasting<span className="md:hidden">AI Integrations</span></span>
           </div>
         </div>
         <div className="px-6 flex flex-col justify-end">
@@ -208,7 +208,7 @@ export default function Pricing() {
             <svg className="shrink-0 fill-purple-500 mr-3" xmlns="http://www.w3.org/2000/svg" width="12" height="9">
               <path d="M10.28.28 3.989 6.575 1.695 4.28A1 1 0 0 0 .28 5.695l3 3a1 1 0 0 0 1.414 0l7-7A1 1 0 0 0 10.28.28Z" />
             </svg>
-            <span>Custom report generation<span className="md:hidden">AI Integrations</span></span>
+            <span>Custom Report Generation<span className="md:hidden">AI Integrations</span></span>
           </div>
         </div>
         {/* # Features */}
